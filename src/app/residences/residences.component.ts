4fd3e660fccc2f1d89c32e35b00cbdac4a63b200
amal-selmi <amalselmi094@gmail.com>
2024-11-21 12:38:18 +0100

    ajouter des images

--- src/app/residences/residences.component.ts
+++ src/app/residences/residences.component.ts
@@ -8,7 +8,7 @@ import { Residence } from '../core/models/residence.model';
 })
 export class ResidencesComponent {
   listResidences: Residence[] = [
-    { id: 1, name: "El fel", address: "Borj Cedria", image: "../../assets/images/R1.jpeg", status: "Disponible" },
+    { id: 1, name: "El fel", address: "Borj Cedria", image: "../../assets/images/R1.jpg", status: "Disponible" },
     { id: 2, name: "El yasmine", address: "Ezzahra", image: "../../assets/images/R2.jpg", status: "Disponible" },
     { id: 3, name: "El Arij", address: "Rades", image: "../../assets/images/R3.jpg", status: "Vendu" },
     { id: 4, name: "El Anber", address: "inconnu", image: "../../assets/images/R4.jpg", status: "En Construction" }
